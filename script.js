c0f9400379ae932d788ec9c3abc9144d523eafe6
Laury Ritter <lauryritter@yahoo.com>
2021-09-23 08:46:22 -0700

    added prompts

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  //var password = generatePassword();
+  var password = generatePassword(); 
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -11,7 +11,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
 
 // Add function variables
 
@@ -34,3 +34,33 @@ function getSymbol() {
     const symbol = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
     return symbol[Math.floor(Math.random() * symbol.length)]
 }
+
+//Add prompts
+      var passLength = ""; {
+      passLength = prompt("How many characters would you like your password to contain?");
+      if (passLength <= 7) {
+        passLength = alert("Password must be at least 8 characters.")
+        }
+      if (passLength >= 129) {
+        passLength = alert("Password must be fewer than 128 characters.")
+        }
+      if (passLength == null){
+        passlength = alert("Please enter the number of charcters for your password.")
+      } 
+    }
+
+    var passSpecial = ""; {
+        passSpecial = confirm("Click OK to confirm special characters.")
+    }
+
+    var passNumber = ""; {
+        passNumber = confirm("Click OK to confirm numeric characters.")
+    }
+
+    var passLowercase = ""; {
+        passLowercase = confirm("Click OK to confirm lowercase characters.")
+    }
+
+    var passUppercase = ""; {
+        passUppercase = confirm("Click OK to confirm uppercase characters.")
+    }
