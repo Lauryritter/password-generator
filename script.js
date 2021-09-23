0a02d35cf08cbc098e004a2cfea8d56cfaefbd34
Laury Ritter <lauryritter@yahoo.com>
2021-09-23 06:50:33 -0700

    added function variables

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -12,3 +12,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Add function variables
+
+function getLowercase() {
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    return lowercase[Math.floor(Math.random() * lowercase.length)]
+}
+
+function getUppercase() {
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return uppercase[Math.floor(Math.random() * uppercase.length)]
+}
+
+function getNumber() {
+    const number = "0123456789";
+    return number[Math.floor(Math.random() * number.length)]
+}
+
+function getSymbol() {
+    const symbol = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+    return symbol[Math.floor(Math.random() * symbol.length)]
+}
