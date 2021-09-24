96ba3ae08dd1e460c37f81db4c7327b7beba2185
Laury Ritter <lauryritter@yahoo.com>
2021-09-23 18:55:33 -0700

    final js

--- script.js
+++ script.js
@@ -1,60 +1,99 @@
 //Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateButton = document.getElementById("generateBtn");
 
-//Add function variables
-const options = {
-  lowercase: "abcdefghijklmnopqrstuvwxyz",
-  uppercase: "ABCDEFGHIJKLMNOPQRUSTUVWXYZ",
-  numbers: "0123456789",
-  symbols: " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
-};
+//Added arrays
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbols = [" ", "!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
+//Add EventListener
 generateBtn.addEventListener("click", writePassword)
 
 //Write password to the #password input
 function writePassword() {
-var password = generatePassword(); 
-var passwordText = document.querySelector("#password");
+  var password = generatePassword(); 
+  var passwordText = document.querySelector("#password");
 
-passwordText.value = password;
+  passwordText.value = password;
 } 
+
+//variables for prompts
+
+var passLength;
+var passLength = "";
+var passSpecial;
+var passLowercase;
+var passUppercase;
 
 //Add prompts
 function generatePassword() {
     var passLength = prompt("How many characters would you like your password to contain?");{
-      if (passLength <= 7) 
-      passLength = window.alert("Password must be at least 8 characters.");
+      while (passLength <= 7) {
+      passLength = alert("Password must be at least 8 characters.");
+      var passLength = prompt("How many characters would you like your password to contain?");
+      }
+    }      
+      while (passLength >= 129) {
+      passLength = alert("Password must be fewer than 128 characters.");
+      var passLength = prompt("How many characters would you like your password to contain?");
     }
-      if (passLength >= 129) {
-      passLength = window.alert("Password must be fewer than 128 characters.");
-    }
-      if (passLength == null){
-      passlength = window.alert("Please enter the number of charcters for your password.");
+      while (passLength == null){
+      passLength = alert("Please enter the number of charcters for your password.");
+      var passLength = prompt("How many characters would you like your password to contain?");
     } 
-    var passSpecial = window.confirm("Click OK to confirm special characters.");
-      if (passSpecial) {
-      password += options.symbols;
-    };
-
+    var passSpecial = confirm("Click OK to confirm special characters.");
+      
     var passNumber = window.confirm("Click OK to confirm numeric characters.");
-      if (passNumber) {
-      password += options.numbers;
-  };
-
+      
     var passLowercase = window.confirm("Click OK to confirm lowercase characters.");
-      if (passNumber) {
-      password += options.lowercase;
-  };
-
+    
     var passUppercase = window.confirm("Click OK to confirm uppercase characters.");
-      if (passNumber) {
-      password += options.uppercase;
-  };
-  
-  var password = "";
-  for (let i = 0; i < passLength; i++) {
-    password += generatePassword(Math.floor(Math.random() * generatePassword.passLength))
-  }
- console.log (writePassword())
+    
+    //Enter minimum for numbers, lowercase, uppercase and special characters.
+    var minimumChar;
+    minimumChar = [];
 
-}
+
+
+    //Run if scenarios
+
+    if(passSpecial === true){
+      minimumChar = symbols;
+    }
+    if (passNumber === true){
+      minimumChar = numbers;
+    }
+    if (passLowercase === true){
+      minimumChar = lowercase;
+    }
+
+    if (passUppercase === true){
+      minimumChar = uppercase;
+    }
+
+    if (passSpecial){
+      minimumChar = minimumChar.concat(symbols)
+    }
+
+    if (passNumber){
+      minimumChar = minimumChar.concat(numbers)
+    }
+
+    if (passLowercase){
+      minimumChar = minimumChar.concat(lowercase)
+    }
+
+    if (passUppercase){
+      minimumChar = minimumChar.concat(uppercase)
+    }
+
+    var ranPassword = ""
+
+    //loop random characters
+
+    for (var i = passLength; i< length; i++) 
+    writePassword = ranPassword + minimumChar[Math.floor(math.random() * minimumChar.length)]
+
+    return ranPassword;
+  }
